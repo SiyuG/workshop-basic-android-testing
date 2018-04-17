98077b636bc40cf4aba4551a9e6c488990eaf394
SiyuG <siyu_g@cmu.ac.th>
2018-04-17 16:45:02 +0700

    Second test

--- app/src/androidTest/java/workshop/testing/data/model/RecipeTest.java
+++ app/src/androidTest/java/workshop/testing/data/model/RecipeTest.java
@@ -17,11 +17,29 @@ public class RecipeTest {
         assertNotNull(recipe);
         assertEquals("water", recipe.id);
         assertEquals("Water", recipe.title);
-        assertEquals("Water", "Put glass under tao.Open tap.Close tap.Drink.", recipe.description);
-    
+        assertEquals("Put glass under tap.Open tap.Close tap.Drink."
+                ,recipe.description);
 
 
 
+
+    }public  void i_can_read_data_from_mixed(){
+        InputStream stream
+                = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
+        Recipe recipe = Recipe.readFromStream(stream);
+
+        assertNotNull(recipe);
+        assertEquals("punch",recipe.id);
+        assertEquals("Punch",recipe.title);
+        assertEquals("id=punch\n" +
+                "Juice of 3 lemons\n" +
+                "1 orange\n" +
+                "title=Punch\n" +
+                "1 pint grape juice\n" +
+                "1 cup sugar\n" +
+                "1 cup water\n" +
+                "1 pine apple juice\n" +
+                "Mix all together and strain. Add large piece of ice.",recipe.description);
 
     }
 
